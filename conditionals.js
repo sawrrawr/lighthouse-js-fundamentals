dd1c157b3bbf554c9e85b9b979a86c39d5bf60fe
Sarah Melanie White <sawrrawr@gmail.com>
2021-12-19 11:52:35 -0330

    added more examples of conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,17 @@
+const raining = true;
+const cold = false;
 
+if (raining) {
+  console.log('Don\'t forget your umbrella');
+}
 
-const temperature = 21;
+if (cold) {
+  console.log('Make sure you pick out a scarf!');
+}
+
+console.log('Now you\'re ready to go outside!');
+
+const temperature = 50;
 
 if (temperature < 0) {
   console.log('Make sure you pick out a scarf!');
@@ -11,3 +22,18 @@ if (temperature < 0) {
 }
 
 console.log('Now you\'re ready to go outside!');
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log('You are eligible to vote.');
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log('Maybe going outside isn\'t such a great idea.');
+}
+
+if (!raining) {
+  console.log('Leave your umbrella at home!');
+}
